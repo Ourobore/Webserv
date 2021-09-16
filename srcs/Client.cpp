6985e6c08f9046fcb5613b919d42428d7b09de59
Ourobore <chaprenet.loic@gmail.com>
2021-09-16 16:37:00 +0200

    Added compile_flags.txt for headers recognition in clangd

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,25 +1,19 @@
-#include "../includes/Client.hpp"
+#include "Client.hpp"
 
 Client::Client(int domain, int type, int protocol, int port, u_long interface)
-    : sock(domain, type, protocol, port, interface)
-{
-    int return_value = 0;
-    return_value = connect(
-        sock.get_fd(), reinterpret_cast<struct sockaddr*>(&sock.get_address()),
-        sizeof(sock.get_address()));
-    check_error(return_value, "client connection failed");
+    : sock(domain, type, protocol, port, interface) {
+  int return_value = 0;
+  return_value = connect(
+      sock.get_fd(), reinterpret_cast<struct sockaddr *>(&sock.get_address()),
+      sizeof(sock.get_address()));
+  check_error(return_value, "client connection failed");
 }
 
-void Client::check_error(int value, const std::string message)
-{
-    if (value < 0)
-    {
-        std::cerr << "Error: " << message << std::endl;
-        exit(EXIT_FAILURE);
-    }
+void Client::check_error(int value, const std::string message) {
+  if (value < 0) {
+    std::cerr << "Error: " << message << std::endl;
+    exit(EXIT_FAILURE);
+  }
 }
 
-Socket& Client::get_socket()
-{
-    return (sock);
-}
+Socket &Client::get_socket() { return (sock); }
