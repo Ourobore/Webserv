f2a7c676e69db2c126bd964eba77309658a1759a
Jonny Saykosy <josaykos@student.42.fr>
2021-10-07 13:30:28 +0000

    update main.cpp. Unused comments.

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,12 @@ int main(int argc, char** argv)
     std::vector<Config> configs = main_parsing_config(argc, argv);
     if (configs.empty())
         return (-1);
-    std::cout << "configs[0].get_root(): " << configs[0].get_root()
-              << std::endl;
 
     // Create a new server
-    // TODO: Create a new server with data from configs
     Webserv web;
 
     std::vector<Config>::iterator it;
     for (it = configs.begin(); it != configs.end(); ++it)
-        // web.create_server(AF_INET, SOCK_STREAM, 0, it->get_port(),
-        // INADDR_ANY);
         web.create_server(*it);
 
     web.start();
