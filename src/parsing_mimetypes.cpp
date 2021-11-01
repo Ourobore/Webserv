5b046ad3c825cca5bfdec77524e3ee69c77c237e
Jonny Saykosy <josayko@pm.me>
2021-11-01 13:03:59 +0100

    Update: Parsing mimetypes...

--- src/parsing_mimetypes.cpp
+++ src/parsing_mimetypes.cpp
@@ -1,7 +1,8 @@
 #include "parsing_mimetypes.hpp"
 
-std::map<std::string, std::string> parsing_mimetypes(const char* fiename)
+std::map<std::string, std::string> parsing_mimetypes(const char* filename)
 {
     std::map<std::string, std::string> mimetypes;
+    (void)filename;
     return mimetypes;
 }
